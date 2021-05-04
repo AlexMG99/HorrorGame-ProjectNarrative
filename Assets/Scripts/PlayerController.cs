9276862652883a06d0ef37a7497eee649fd380c9
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2021-05-04 19:46:51 +0200

    Finished player PickUp

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,11 +13,13 @@ public class PlayerController : MonoBehaviour
 
     List<string> inventory;
 
-
     CharacterController controller;
     CinemachineVirtualCamera virtualCamera;
 
-    // Start is called before the first frame update
+    public GameObject middlePoint;
+    public Text objectText;
+
+   
     void Awake()
     {
         controller = GetComponent<CharacterController>();
@@ -25,13 +28,19 @@ public class PlayerController : MonoBehaviour
         inventory = new List<string>();
     }
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("RayCast");
+    }
+
     // Update is called once per frame
     void Update()
     {
         PlayerMovement();
 
-        if (Input.GetKeyDown("E"))
-            StartCoroutine("RayCastToPick");
+        if (Input.GetKeyDown("e"))
+            PickObject();
     }
 
     // Player Movement Input
@@ -40,34 +49,54 @@ public class PlayerController : MonoBehaviour
         // Player acceleration
         if (speed < maxSpeed && (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0))
             speed += acceleration * Time.deltaTime;
-        else if(Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
-            speed = 1.0f;
+        else if(speed > 1.0 && Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+            speed -= acceleration * Time.deltaTime;
 
         // Movement
         Vector3 move = Vector3.zero;
         move += Input.GetAxis("Vertical") * virtualCamera.gameObject.transform.forward;
         move += Input.GetAxis("Horizontal") * virtualCamera.gameObject.transform.right;
         move.y = 0.0f;
-
-
+        //move += Physics.gravity * 0.5f;
         controller.Move(move * Time.deltaTime * speed);
+
+
 
         Vector3 direction =  Vector3.Lerp(gameObject.transform.forward, virtualCamera.gameObject.transform.forward, camMovSpeed * Time.deltaTime);
         gameObject.transform.forward = new Vector3(direction.x, 0.0f, direction.z);
     }
 
-    IEnumerator RayCastToPick()
+    IEnumerator RayCast()
     {
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, transform.forward, out hit, 20.0f))
+        if (Physics.Raycast(virtualCamera.gameObject.transform.position, virtualCamera.gameObject.transform.forward, out hit, 100.0f))
         {
+            middlePoint.SetActive(false);
+            objectText.text = "";
+
             if (hit.transform.gameObject.CompareTag("Pick"))
             {
-                inventory.Add(hit.transform.gameObject.name);
-                Destroy(hit.transform.gameObject);
+                middlePoint.SetActive(true);
+                objectText.text = hit.transform.gameObject.name;
             }
         }
 
-        yield return null;
+        yield return new WaitForSeconds(.5f);
+        StartCoroutine("RayCast");
+    }
+
+    void PickObject()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(virtualCamera.gameObject.transform.position, virtualCamera.gameObject.transform.forward, out hit, 100.0f))
+        {
+            Debug.DrawRay(virtualCamera.gameObject.transform.position, virtualCamera.gameObject.transform.forward * 10000, Color.green, 5);
+            if (hit.transform.gameObject.CompareTag("Pick"))
+            {
+                inventory.Add(hit.transform.gameObject.name);
+                Debug.Log("Object: " + inventory[0]);
+                Destroy(hit.transform.gameObject);
+            }
+        }
     }
 }
