06478ea53bd6c96570f44e6a27bf044d73e465ee
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2021-05-04 17:52:33 +0200

    Added correct movement to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float playerSpeed = 2.0f;
+    public float speed = 0.0f;
+    public float acceleration = 0.3f;
+    public float maxSpeed = 5.0f;
     public float camMovSpeed = 5.0f;
+
+
     CharacterController controller;
+    CinemachineVirtualCamera virtualCamera;
 
     // Start is called before the first frame update
     void Awake()
     {
         controller = GetComponent<CharacterController>();
+        virtualCamera = GameObject.Find("PlayerCamera").GetComponent<CinemachineVirtualCamera>();
     }
 
     // Update is called once per frame
@@ -23,9 +30,13 @@ public class PlayerController : MonoBehaviour
     // Player Movement Input
     void PlayerMovement()
     {
+        if (speed < maxSpeed)
+            speed += acceleration * Time.deltaTime;
+        
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        controller.Move(move * Time.deltaTime * speed);
 
-        gameObject.transform.forward = Vector3.Lerp(gameObject.transform.forward, move, camMovSpeed * Time.deltaTime);
+        Vector3 direction =  Vector3.Lerp(gameObject.transform.forward, virtualCamera.gameObject.transform.forward, camMovSpeed * Time.deltaTime);
+        gameObject.transform.forward = new Vector3(direction.x, 0.0f, direction.z);
     }
 }
