8014e3ac2bc09926c0923f5497b9281643bb6a67
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2021-05-04 20:10:00 +0200

    Organized code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-using UnityEngine.UI;
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,36 +11,17 @@ public class PlayerController : MonoBehaviour
     public float maxSpeed = 5.0f;
     public float camMovSpeed = 5.0f;
 
-    List<string> inventory;
-
     CharacterController controller;
-    CinemachineVirtualCamera virtualCamera;
-
-    public GameObject middlePoint;
-    public Text objectText;
-
    
     void Awake()
     {
         controller = GetComponent<CharacterController>();
-        virtualCamera = GameObject.Find("PlayerCamera").GetComponent<CinemachineVirtualCamera>();
-
-        inventory = new List<string>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine("RayCast");
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMovement();
-
-        if (Input.GetKeyDown("e"))
-            PickObject();
     }
 
     // Player Movement Input
@@ -54,49 +35,15 @@ public class PlayerController : MonoBehaviour
 
         // Movement
         Vector3 move = Vector3.zero;
-        move += Input.GetAxis("Vertical") * virtualCamera.gameObject.transform.forward;
-        move += Input.GetAxis("Horizontal") * virtualCamera.gameObject.transform.right;
+        move += Input.GetAxis("Vertical") * GameManager.Instance.GetVirtualCamera().transform.forward;
+        move += Input.GetAxis("Horizontal") * GameManager.Instance.GetVirtualCamera().transform.right;
         move.y = 0.0f;
         //move += Physics.gravity * 0.5f;
         controller.Move(move * Time.deltaTime * speed);
 
-
-
-        Vector3 direction =  Vector3.Lerp(gameObject.transform.forward, virtualCamera.gameObject.transform.forward, camMovSpeed * Time.deltaTime);
+        Vector3 direction =  Vector3.Lerp(gameObject.transform.forward, GameManager.Instance.GetVirtualCamera().transform.forward, camMovSpeed * Time.deltaTime);
         gameObject.transform.forward = new Vector3(direction.x, 0.0f, direction.z);
     }
 
-    IEnumerator RayCast()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(virtualCamera.gameObject.transform.position, virtualCamera.gameObject.transform.forward, out hit, 100.0f))
-        {
-            middlePoint.SetActive(false);
-            objectText.text = "";
 
-            if (hit.transform.gameObject.CompareTag("Pick"))
-            {
-                middlePoint.SetActive(true);
-                objectText.text = hit.transform.gameObject.name;
-            }
-        }
-
-        yield return new WaitForSeconds(.5f);
-        StartCoroutine("RayCast");
-    }
-
-    void PickObject()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(virtualCamera.gameObject.transform.position, virtualCamera.gameObject.transform.forward, out hit, 100.0f))
-        {
-            Debug.DrawRay(virtualCamera.gameObject.transform.position, virtualCamera.gameObject.transform.forward * 10000, Color.green, 5);
-            if (hit.transform.gameObject.CompareTag("Pick"))
-            {
-                inventory.Add(hit.transform.gameObject.name);
-                Debug.Log("Object: " + inventory[0]);
-                Destroy(hit.transform.gameObject);
-            }
-        }
-    }
 }
