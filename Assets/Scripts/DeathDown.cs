bac084de1e782657a791877a2bd64508ae257942
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2021-06-12 20:38:25 +0200

    Worked in level 2

---
+++ Assets/Scripts/DeathDown.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class DeathDown : MonoBehaviour
+{
+    public string sceneName;
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if(hit.gameObject.CompareTag("Player"))
+        {
+            EditorSceneManager.LoadScene(sceneName);
+        }
+    }
+}
